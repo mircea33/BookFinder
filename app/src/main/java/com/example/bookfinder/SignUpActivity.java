0bf3e3eb6eed8d27f22e380bcbf33c9c269bf0bf
mircea33 <72373515+mircea33@users.noreply.github.com>
2024-05-08 08:43:51 +0300

    [IMP] Add warning in editboxes

--- app/src/main/java/com/example/bookfinder/SignUpActivity.java
+++ app/src/main/java/com/example/bookfinder/SignUpActivity.java
@@ -16,11 +16,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText signupName, signupEmail, signupUsername, signupPassword;
-    TextView loginRedirectText;
-    Button signupButton;
-    FirebaseDatabase database;
-    DatabaseReference reference;
+    private EditText signupName, signupEmail, signupUsername, signupPassword;
+    private TextView loginRedirectText;
+    private Button signupButton;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +41,23 @@ public class SignUpActivity extends AppCompatActivity {
                 database = FirebaseDatabase.getInstance();
                 reference = database.getReference("users");
 
+                EditText[] editTextBoxes = new EditText[]{signupName, signupEmail, signupUsername, signupPassword};
+                if (isAlertRequired(editTextBoxes))
+                {
+                    return;
+                }
+
                 String name = signupName.getText().toString();
                 String email = signupEmail.getText().toString();
                 String username = signupUsername.getText().toString();
                 String password = signupPassword.getText().toString();
+
+                if (!email.contains("@"))
+                {
+                    signupEmail.setError("Invalid credentials");
+                    signupEmail.requestFocus();
+                    return;
+                }
 
                 HelperClass helperClass = new HelperClass(name, email, username, password);
                 reference.child(username).setValue(helperClass);
@@ -58,9 +71,26 @@ public class SignUpActivity extends AppCompatActivity {
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean isAlertRequired(EditText[] editTexts)
+    {
+        boolean alertRequired = false;
+        for (EditText editText : editTexts)
+        {
+            if (editText.getText().toString().isEmpty())
+            {
+                editText.setError("Invalid credentials");
+                editText.requestFocus();
+                alertRequired = true;
+            }
+        }
+
+        return alertRequired;
     }
 }

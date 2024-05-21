cfb8d5a691e1c25dc194e4c20caf77a09fe8eddf
mircea33 <72373515+mircea33@users.noreply.github.com>
2024-05-21 09:19:26 +0300

    [IMP] Add log out option

--- app/src/main/java/com/example/bookfinder/MainActivity.java
+++ app/src/main/java/com/example/bookfinder/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bookfinder;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
--- app/src/main/java/com/example/bookfinder/ProfileActivity.java
+++ app/src/main/java/com/example/bookfinder/ProfileActivity.java
@@ -1,27 +1,55 @@
 package com.example.bookfinder;
 
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.bookfinder.databinding.ActivityProfileBinding;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
-    private boolean continueLogOut = false;
+    private Button buttonCapture;
+    private TextView textViewData;
+    private Bitmap bitmap;
+    private static final int REQUEST_CAMERA_CODE = 100;;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        buttonCapture = findViewById(R.id.ocr_scann);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_CODE);
+        }
+
+        buttonCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(ProfileActivity.this);
+            }
+        });
 
         replaceGragment(new HomeFragment());
         binding.bottomNavigationView.setBackground(null);
@@ -42,13 +70,40 @@ public class ProfileActivity extends AppCompatActivity {
             }
             else if (itemId == R.id.logout)
             {
-                if (logoutPopUp())
-                {
-                    finish();
-                }
+                logoutPopUp();
             }
             return true;
         });
+    }
+
+    private void getTextFromImage(Bitmap bitmap)
+    {
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if(!recognizer.isOperational())
+        {
+            Toast.makeText(this, "Error Ocurred !!!", Toast.LENGTH_SHORT);
+        }
+        else
+        {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < textBlockSparseArray.size(); i++)
+            {
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+
+            textViewData.setText(stringBuilder.toString());
+            buttonCapture.setText("Retake");
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+            builder.setTitle(stringBuilder);
+
+            builder.setNegativeButton("No", (dialogInterface, i) -> {});
+            builder.show();
+        }
     }
 
     private void replaceGragment(Fragment fragment)
@@ -59,17 +114,16 @@ public class ProfileActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    private boolean logoutPopUp()
+    private void logoutPopUp()
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
 
         builder.setTitle("Logout");
         builder.setMessage("Are you sure you want to log out?");
 
-        builder.setNegativeButton("No", (dialogInterface, i) -> continueLogOut = false);
-        builder.setPositiveButton("Yes", (dialogInterface, i) -> continueLogOut = true);
+        builder.setNegativeButton("No", (dialogInterface, i) -> {});
+        builder.setPositiveButton("Yes", (dialogInterface, i) -> finish());
 
         builder.show();
-        return continueLogOut;
     }
 }
